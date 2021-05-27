b00c91667041dd57f6de9bd5551006ea537468ae
MohamedAlsony <mohamedahmedelsony@gmail.com>
2021-05-28 01:40:56 +0200

    '#'

--- vaccination/settings.py
+++ vaccination/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-al0kl*gy-b^f6$)ero1@t)noqq3o3#nm6a2d$%wq9vzqtab!f='
-
+#SECRET_KEY = 'django-insecure-al0kl*gy-b^f6$)ero1@t)noqq3o3#nm6a2d$%wq9vzqtab!f='
+SECRET_KEY = '!p1ai_b373g3bmz-**m@%h9+0_8xm7*41etdbi+t266-mogm08'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
