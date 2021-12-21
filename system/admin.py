7c8741d0db7772861e959cc62b3a596253ecca55
MohamedAlsony <mohamedahmedelsony@gmail.com>
2021-12-21 21:15:13 +0200

    '#'

--- system/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-admin.site.register(Parent)
-admin.site.register(Child)
-admin.site.register(Vaccine)
-admin.site.register(ChildVaccine)
